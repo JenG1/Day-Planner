c37f88ac62b6eb93233043f642c7aef09483a4d7
JGen1 <jengenes01@gmail.com>
2019-11-16 18:03:55 -0500

    Added logic for saving entries in local storage. Updated README, added function set interval to refresh current date.

--- Assets/script.js
+++ Assets/script.js
@@ -1,22 +1,71 @@
-
-var savedEntries = [];
-let date = moment().format('dddd MMMM Do');
-console.log(date);
+// var value = ""
+var date = moment().format('dddd MMMM Do');
 $('#currentDay').text(date);
+var currentTime = moment().format("h:mma"); 
+console.log(currentTime);
 
-$('#saveBtn').on('click', function(){
-    $('.description').each(function(){    
-        var id = $(this).attr('id');
-        var value = $(this).val();
-       localStorage.setItem(id, value);
-    });   
+//Call functio t update date
+setInterval(checkTime, 600000);
+//Click Events for each timeblock
+$(document).one('ready', function () {
+    localStorage.clear();
+});
+//9AM
+$('#btn1').on('click', function () {
+    var value1 = $("#userEntry1").val();
+    localStorage.setItem("Entry1", value1);
+});
+//10AM
+$('#btn2').on('click', function () {
+    var value2 = $("#userEntry2").val();
+    localStorage.setItem("Entry2", value2);
+});
+//11AM
+$('#btn3').on('click', function () {
+    var value3 = $("#userEntry3").val();
+    localStorage.setItem("Entry3", value3);
+});
+//12PM
+$('#btn4').on('click', function () {
+    var value4 = $("#userEntry4").val();
+    localStorage.setItem("Entry4", value4);
+});
+//1PM
+$('#btn5').on('click', function () {
+    var value5 = $("#userEntry5").val();
+    localStorage.setItem("Entry5", value5);
+});
+//2PM
+$('#btn6').on('click', function () {
+    var value6 = $("#userEntry6").val();
+    localStorage.setItem("Entry6", value6);
+});
+//3PMM
+$('#btn7').on('click', function () {
+    var value7 = $("#userEntry7").val();
+    localStorage.setItem("Entry7", value7);
+});
+//4PM
+$('#btn8').on('click', function () {
+    var value8 = $("#userEntry8").val();
+    localStorage.setItem("Entry8", value8);
+});
+//5PM
+$('#btn9').on('click', function () {
+    var value9 = $("#userEntry9").val();
+    localStorage.setItem("Entry9", value9);
 });
 
 
+//Function retrieves current date in 'dddd MMMM Do' format and displays it on paege.
+function checkTime() {
+    let date = moment().format('dddd MMMM Do');
+    $('#currentDay').text(date);
+}
 
-//User entry field has a different background color depending on whether the entry is saved in the past present or future.
-// let hour = moment().format('h a');
-// console.log(hour);
+var endTime = "9:00a"
+//Function checks curernt time and changes the background to represent whether it is in the past, present, or future.
+if (currentTime > endTime){
+    // Add class to change color.
+  }
 
-// console.log(moment('h').isAfter('h')); // returns false
-// console.log(moment('2010-11-20').isAfter('2010-10-19')); // returns true
